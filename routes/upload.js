14d9971f0495561a56623a094d5a041ee273002e
queanh0220 <queanh0220@gmail.com>
2022-05-24 20:08:25 +0700

    fix upload

--- routes/upload.js
+++ routes/upload.js
@@ -33,7 +33,7 @@ uploadRouter.post("/",upload.single("file"), async (req, res) => {
         .collection("uploads")
         .insertOne(file);
       const value = result.insertedId;
-      res.send(process.env.URLSERVER+value);
+      res.send(`${process.env.URLSERVER}upload/${value}`);
 });
 
 uploadRouter.get("/:id", async (req, res) => {
